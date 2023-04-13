63826673a88e2a2d8788b16a4eaa57b282a4529d
y-majid16 <yaumulmajid@gmail.com>
2023-04-13 12:52:18 +0700

    update responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
